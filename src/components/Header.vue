37bf190e89400272503d3fa9f57072e4d0cf9af4
Andrien Wiandyano <arnnied03@gmail.com>
2021-11-06 17:06:14 +0700

    components/Header: resized padding size

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,22 @@
 <script setup></script>
 
 <template>
-	<div
-		class="
+  <div class="flex flex-col">
+    <div class="
 			flex flex-row
 			items-center
-			p-8
+			p-4
 			bg-gray-800
 			justify-center
 			space-x-2
-		"
-	>
-		<img src="/src/assets/logo.png" alt="Vue logo" class="w-16 h-16" />
-		<h2 class="font-bold text-2xl text-white">Conway's Game of Life</h2>
-	</div>
+		">
+      <img
+        src="/src/assets/logo.png"
+        alt="Vue logo"
+        class="w-16 h-16"
+      />
+      <h2 class="font-bold text-2xl text-white">Conway's Game of Life</h2>
+    </div>
+  </div>
+
 </template>
