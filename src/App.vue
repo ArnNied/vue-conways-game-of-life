9f7b89e38722d3437b000f400609d3d36b585405
Andrien Wiandyano <arnnied03@gmail.com>
2021-11-06 20:11:29 +0700

    src: added and implemented Control

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,34 @@ import { ref } from "vue";
 
 import Board from "/src/components/Board.vue";
 import Config from "/src/components/Config.vue";
+import Control from "/src/components/Control.vue";
 import Header from "/src/components/Header.vue";
 
 const config = ref(null);
+
+const simulationProcess = ref(false);
+const currentCycle = ref(0);
+
+function simulationControl() {
+  console.log("simulationProcess");
+  simulationProcess.value = !simulationProcess.value;
+}
+
+function clearOrReset() {
+  console.log("creset");
+}
 </script>
 
 <template>
   <div class="flex flex-col">
     <Header />
     <Config ref="config" />
+    <Control
+      @simulation-control="simulationControl"
+      @creset="clearOrReset"
+      :simulation-process="simulationProcess"
+      :current-cycle="currentCycle"
+    />
     <Board :config="config" />
   </div>
 </template>
---
+++ src/components/Control.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  simulationProcess: {
+    type: Boolean,
+    default: false,
+  },
+  currentCycle: {
+    type: Number,
+    default: 0,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-row justify-center p-4 space-x-4">
+    <div class="flex">
+      <button
+        @click="$emit('simulation-control')"
+        class="p-2 bg-gray-900 text-white rounded"
+      >
+        {{simulationProcess ? 'Pause' : 'Start'}}
+      </button>
+    </div>
+    <div class="flex">
+      <button
+        @click="$emit('creset')"
+        class="p-2 bg-gray-900 text-white rounded"
+      >
+        {{currentCycle ? 'Reset' : 'Clear'}}
+      </button>
+    </div>
+
+  </div>
+</template>
