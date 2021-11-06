278287cc1531625030a394bdd5570d6941b577b6
Andrien Wiandyano <arnnied03@gmail.com>
2021-11-06 16:29:57 +0700

    components: added component Config

---
+++ src/components/Config.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from "vue";
+
+const board = ref({ width: 0, height: 0 });
+const localBoard = ref({ width: 0, height: 0 });
+
+function createBoard() {
+  board.value.width = localBoard.value.width;
+  board.value.height = localBoard.value.height;
+}
+
+defineExpose({
+  board,
+});
+</script>
+
+<template>
+  <div class="flex flex-row justify-center p-8 space-x-8">
+    <div class="flex flex-row items-center space-x-2">
+      <p class="text-white">Width:</p>
+      <input
+        v-model="localBoard.width"
+        type="number"
+        name="config-width"
+        id="config-width"
+        placeholder="Board Width"
+        class="p-1 rounded-sm"
+      />
+    </div>
+    <div class="flex flex-row items-center space-x-2">
+      <p class="text-white">Height:</p>
+      <input
+        v-model="localBoard.height"
+        type="number"
+        name="config-height"
+        id="config-Height"
+        placeholder="Board Height"
+        class="p-1 rounded-sm"
+      />
+    </div>
+    <div class="flex items-center">
+      <button
+        @click="createBoard"
+        class="p-2 bg-gray-800 text-white"
+      >
+        Build
+      </button>
+    </div>
+  </div>
+</template>
