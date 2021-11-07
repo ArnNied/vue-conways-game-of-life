203c66fc329b0dfe85227fbaf5443f239d08eef6
Andrien Wiandyano <arnnied03@gmail.com>
2021-11-07 22:49:50 +0700

    components/Header: added links to header section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,21 @@
       />
       <h2 class="font-bold text-2xl text-white">Conway's Game of Life</h2>
     </div>
+    <div class="flex flex-row justify-center p-1 bg-gray-700 space-x-8">
+      <a
+        href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+        target="_blank"
+        class="text-sm text-white hover:underline"
+      >
+        What is "John Conway's Game of Life"?
+      </a>
+      <a
+        href="https://github.com/ArnNied/vue-conways-game-of-life"
+        target="_blank"
+        class="text-sm text-white hover:underline"
+      >
+        Source Code
+      </a>
+    </div>
   </div>
-
 </template>
